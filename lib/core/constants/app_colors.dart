dab4b84631831c079f71e201a63e20166e671914
Isabella Estella <bellahestela@gmail.com>
2026-06-14 17:10:28 -0300

    Push inicial

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  // ============================================
+  // cores gerais
+  // ============================================
+
+  static const background = Color(0xFFE1D9CB);
+  static const card = Color(0xFFF4EEE8);
+  static const green = Color(0xFF7E8462);
+
+
+  // botoes -----
+
+  static const buttonBrown = Color(0xFF987A64);
+
+  // fitas -----
+  static const fundoFitaVerde = Color(0xFFDFE2D4);
+  static const listrasFitaVerde = Color.fromARGB(70, 32, 66, 31);
+
+
+  // ============================================
+
+
+}
